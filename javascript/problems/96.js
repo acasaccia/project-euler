f5c7abfcd12b8bc68f7e6734a9e8a36baa3d659a
Andrea Casaccia <ANDREACA@amdocs.com>
2015-12-22 18:07:04 +0000

    Problem 96 bare implementation

--- javascript/problems/96.js
+++ javascript/problems/96.js
@@ -12,3 +12,81 @@
 // the file is the example above). By solving all fifty puzzles find the sum of the 3-digit numbers found in the top
 // left corner of each solution grid; for example, 483 is the 3-digit number found in the top left corner of the
 // solution grid above.
+
+
+function solve(puzzle) {
+
+    var valid_quadrant = function(table, quadrant_id, value) {
+
+    };
+
+    var valid_row = function(table, row_id, value) {
+        var row_begin = row_id*9;
+        var row_end = row_begin+8;
+        for (var i=row_begin; i<=row_end; i++) {
+            if (table[i] === value) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    var valid_column = function(table, column_id, value) {
+        var column_begin = column_id;
+        var row_end = column_id + 72;
+        for (var i=row_begin; i<=row_end; i++) {
+            if (table[i] === value) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    var valid_insertion = function(table, position, value) {
+        var row_id = parseInt(position / 9);
+        var column_id = position % 9;
+        return valid_quadrant(table, quadrant_id, value) &&
+            valid_row(table, row_id, value) &&
+            valid_column(table, column_id, value);
+    };
+
+    var solved = function(puzzle) {
+        return puzzle.indexOf('0') === -1;
+    };
+
+    var get_adjacents = function(puzzle) {
+        var table = puzzle.split('');
+        var adjacents = [];
+        var table_copy;
+        for (var i=0; i<81; i++) {
+            if (table[i] === '0') {
+                for (var j=1; j<=9; j++) {
+                    if (valid_insertion(table, i, String(j))) {
+                        table_copy = table.slice();
+                        table_copy[i] = j;
+                        adjacents.push(table_copy[i].join(''));
+                    }
+                }
+            }
+        }
+    };
+
+    var visited = {};
+
+    var evaluate = function(puzzle) {
+        if (solved(puzzle)) {
+            console.log(puzzle);
+            process.exit();
+        }
+        visited[puzzle] = true;
+        var adjacents = get_adjacents(puzzle);
+        adjacents.forEach(function(adjacent){
+            if (!evaluated[adjacent]) {
+                evaluate(adjacent)
+            }
+        });
+    };
+
+    evaluate(puzzle);
+
+}
