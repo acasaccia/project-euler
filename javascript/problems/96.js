919b49c774cceaa61a245b1a8187fdfd6db690fa
Andrea Casaccia <acasaccia@gmail.com>
2015-12-25 16:59:19 +0000

    Fixed problem 96

--- javascript/problems/96.js
+++ javascript/problems/96.js
@@ -26,101 +26,116 @@
 // left corner of each solution grid; for example, 483 is the 3-digit number found in the top left corner of the
 // solution grid above.
 
+function valid_quadrant(table, quadrant_row_id, quadrant_column_id, value) {
+    var first_cell = quadrant_row_id * 27 + quadrant_column_id * 3;
+    for (var i=0; i<3; i++) {
+        for (var j=0; j<3; j++) {
+            if (table[first_cell+i+j*9] === value) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function valid_row(table, row_id, value) {
+    var row_begin = row_id*9;
+    var row_end = row_begin+8;
+    for (var i=row_begin; i<=row_end; i++) {
+        if (table[i] === value) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function valid_column(table, column_id, value) {
+    var column_begin = column_id;
+    var column_end = column_id + 72;
+    for (var i=column_begin; i<=column_end; i+=9) {
+        if (table[i] === value) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function valid_insertion(table, position, value) {
+    var row_id = parseInt(position / 9);
+    var column_id = position % 9;
+    var quadrant_row_id = parseInt(row_id / 3);
+    var quadrant_column_id = parseInt(column_id / 3);
+    return valid_quadrant(table, quadrant_row_id, quadrant_column_id, value) &&
+        valid_row(table, row_id, value) &&
+        valid_column(table, column_id, value);
+}
+
+function get_valid_moves(puzzle, insertion_index) {
+    var valid_moves = [];
+    for (var i=1; i<=9; i++) {
+        if (valid_insertion(puzzle, insertion_index, i)) {
+            valid_moves.push(i);
+        }
+    }
+    return valid_moves;
+}
+
+function solved(puzzle) {
+    return puzzle.indexOf(0) === -1;
+}
+
+function print_solution(puzzle) {
+    for (var i=0; i<9; i++) {
+        var row = [];
+        for (var j=0; j<3; j++) {
+            row.push(puzzle.slice(i*9+j*3, i*9+j*3+3).join(''));
+        }
+        console.log(row.join(' '));
+        if ((i+1)%3 === 0) {
+            console.log('');
+        }
+    }
+}
+
+var c = 0;
+
+function evaluate(puzzle) {
+    c++;
+    if (solved(puzzle)) {
+        throw new Error(puzzle)
+    }
+    var insertion_index = puzzle.indexOf(0);
+    var valid_values = get_valid_moves(puzzle, insertion_index);
+    valid_values.forEach(function(valid_value) {
+        puzzle[insertion_index] = valid_value;
+        evaluate(puzzle);
+    });
+    puzzle[insertion_index] = 0;
+}
 
 function solve(puzzle) {
-
-    var valid_quadrant = function(table, quadrant_row_id, quadrant_column_id, value) {
-        var first_cell = quadrant_row_id * 27 + quadrant_column_id * 3;
-        for (var i=0; i<3; i++) {
-            for (var j=0; j<3; j++) {
-                if (table[first_cell+i+j*9] === value) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    };
-
-    var valid_row = function(table, row_id, value) {
-        var row_begin = row_id*9;
-        var row_end = row_begin+8;
-        for (var i=row_begin; i<=row_end; i++) {
-            if (table[i] === value) {
-                return false;
-            }
-        }
-        return true;
-    };
-
-    var valid_column = function(table, column_id, value) {
-        var column_begin = column_id;
-        var column_end = column_id + 72;
-        for (var i=column_begin; i<=column_end; i+=9) {
-            if (table[i] === value) {
-                return false;
-            }
-        }
-        return true;
-    };
-
-    var valid_insertion = function(table, position, value) {
-        var row_id = parseInt(position / 9);
-        var column_id = position % 9;
-        var quadrant_row_id = parseInt(row_id / 3);
-        var quadrant_column_id = parseInt(column_id / 3);
-        return valid_quadrant(table, quadrant_row_id, quadrant_column_id, value) &&
-            valid_row(table, row_id, value) &&
-            valid_column(table, column_id, value);
-    };
-
-    var solved = function(puzzle) {
-        return puzzle.indexOf('0') === -1;
-    };
-
-    var get_adjacents = function(puzzle) {
-        var table = puzzle.split('');
-        var adjacents = [];
-        var table_copy, candidate;
-        for (var i=0; i<81; i++) {
-            if (table[i] === '0') {
-                for (var j=1; j<=9; j++) {
-                    if (valid_insertion(table, i, String(j))) {
-                        table_copy = table.slice();
-                        table_copy[i] = j;
-                        candidate = table_copy.join('');
-                        if (!evaluated[candidate]) {
-                            adjacents.push(candidate);
-                        }
-                    }
-                }
-            }
-        }
-        return adjacents;
-    };
-
-    var evaluated = {};
-
-    var evaluate = function(puzzle) {
-        evaluated[puzzle] = true;
-        if (solved(puzzle)) {
-            throw new Error(puzzle)
-        }
-        var adjacents = get_adjacents(puzzle);
-        adjacents.forEach(function(adjacent){
-            if (!evaluated[adjacent]) {
-                evaluate(adjacent)
-            }
-        });
-    };
-
+    var solution;
     try {
         evaluate(puzzle);
     } catch (e) {
-        console.log(e.message);
-        console.log(Object.keys(evaluated).length + " positions evaluated.")
+        solution = e.message.split(',');
+        //print_solution(solution);
     }
-
-
+    //console.log("Evaluated %d positions", c);
+    return solution;
 }
 
-solve('480921657967345821000876493008102900729504108006790200372009014814253769605410082');
+var lines = require('fs').readFileSync('./problems/input/sudoku.txt', 'utf-8').split('\n').filter(Boolean);
+var result = 0, puzzle, solution;
+
+while(lines.length) {
+    lines.shift();
+    puzzle = [];
+    for (var i=0; i<9; i++) {
+        puzzle.push.apply(puzzle, lines.shift().split(''));
+    }
+    solution = solve(puzzle.map(function(v){ return parseInt(v); }));
+    result += parseInt(solution[0] + solution[1] + solution[2]);
+}
+
+console.log(result);
