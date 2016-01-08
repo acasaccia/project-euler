3a9dddc83ef0a199a7cf6c9b4664367e91c1fbd4
Andrea Casaccia <ANDREACA@amdocs.com>
2016-01-08 13:58:54 +0000

    Refactored problem 25 to use a shared BigInt class

--- javascript/problems/25.js
+++ javascript/problems/25.js
@@ -19,34 +19,17 @@
 //
 // What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 
-var n1 = '1',
-    n2 = '1',
+var BigInt = require("../lib/BigInt.js");
+
+var n1 = new BigInt(1),
+    n2 = new BigInt(1),
     c = 2, n;
 
 do {
     c++;
-    n = bigint_sum(n1, n2);
+    n = n1.add(n2);
     n2 = n1;
     n1 = n;
-} while (n.length < 1000);
+} while (n.digits() < 1000);
 
 console.log(c);
-
-function bigint_sum(a, b) {
-    var digits_difference = a.length > b.length;
-    while (digits_difference > 0) {
-        b = '0' + b;
-        digits_difference--;
-    }
-    var a_last_digit, b_last_digit, sum,
-        carry = 0,
-        result = '';
-    for (var i=b.length-1; i>=0; i--) {
-        a_last_digit = parseInt(a.substr(i, 1));
-        b_last_digit = parseInt(b.substr(i, 1));
-        sum = a_last_digit + b_last_digit + carry;
-        carry = parseInt(sum / 10);
-        result = (sum % 10) + result;
-    }
-    return carry > 0 ? '1' + result : result;
-}
