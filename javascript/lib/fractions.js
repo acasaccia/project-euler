61940feb1f8f922a58ef5faaae6b11ae99254b76
Andrea Casaccia <ANDREACA@amdocs.com>
2016-01-12 19:06:29 +0000

    Problem 57 optimizations

--- javascript/lib/fractions.js
+++ javascript/lib/fractions.js
@@ -3,9 +3,9 @@ var mcd = require('./mcd.js');
 exports.sum = function fractions_sum(n1, d1, n2, d2) {
     var n = n1 * d2 + n2 * d1;
     var d = d1 * d2;
-    var _mcd = mcd(n, d);
+    var mcd_n_d = mcd(n, d);
     return {
-        n: n/_mcd,
-        d: d/_mcd
+        n: n/mcd_n_d,
+        d: d/mcd_n_d
     }
 };
--- javascript/problems/57.js
+++ javascript/problems/57.js
@@ -14,7 +14,7 @@
 //
 // In the first one-thousand expansions, how many fractions contain a numerator with more digits than denominator?
 
-var fractions = require("../lib/big_fractions");
+var BigInteger = require("../lib/BigInteger");
 
 var result = 0;
 var denominator_cache = {
@@ -27,19 +27,19 @@ var VERBOSE = false;
 
 for (var i=1; i<=1000; i++) {
     var expansion = expand(i);
-    if (String(expansion.n).length > String(expansion.d).length) {
+    if (expansion.n.length > expansion.d.length) {
         result++;
     }
     if (VERBOSE) {
         var separator = '';
-        for (var j=0; j<String(expansion.d).length; j++) {
+        for (var j=0; j<expansion.d.length; j++) {
             separator += '-';
         }
         console.log("Test n° %d:", i);
         console.log("%s", expansion.n);
         console.log(separator);
         console.log("%s", expansion.d);
-        console.log("%j", String(expansion.n).length > String(expansion.d).length);
+        console.log("%j", expansion.n.length > expansion.d.length);
         console.log("");
     }
 }
@@ -48,7 +48,7 @@ console.log(result);
 
 function expand(n) {
     var den = denominator(n);
-    return fractions.sum("1", "1", den.d, den.n);
+    return big_fractions_sum("1", "1", den.d, den.n);
 }
 
 function denominator(n) {
@@ -56,6 +56,33 @@ function denominator(n) {
         return denominator_cache[n];
     }
     var den = denominator(n-1);
-    denominator_cache[n] = fractions.sum("2", "1", den.d, den.n);
+    denominator_cache[n] = big_fractions_sum("2", "1", den.d, den.n);
     return denominator_cache[n];
+}
+
+/**
+ * Sums two fractions with arbitrary big numerator and denominator as strings
+ * The following implementation leverages the fact that in our application d1 is always going to be = "1" to take
+ * some shortcuts
+ */
+function big_fractions_sum(n1, d1, n2, d2) {
+    var n = BigInteger.sum(BigInteger.multiply(n1, d2), n2);
+    var d = d2;
+    var mcd = big_mcd(n, d);
+    return {
+        n: mcd === '1' ? n : BigInteger.divide(n, mcd),
+        d: mcd === '1' ? d : BigInteger.divide(d, mcd)
+    }
+}
+
+function big_mcd(a, b) {
+    var r;
+    if (b === "0") {
+        return a;
+    }
+    while((r = BigInteger.mod(a, b)) !== "0") {
+        a = b;
+        b = r;
+    }
+    return b;
 }
