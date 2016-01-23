18041dde3df95cda4ff6c5cd61a54bb790c1572e
Andrea Casaccia <acasaccia@gmail.com>
2016-01-23 20:44:14 +0000

    Problem 60 implementation, still not performing well enough

--- javascript/problems/60.js
+++ javascript/problems/60.js
@@ -1,22 +1,26 @@
+// The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating them in any order the result will always be prime. For example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
+// Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
+
 var is_prime = require("../lib/is_prime.js");
 var sieve = require("../lib/sieve.js");
 require("../lib/combinations.js");
 
 var found = false;
-var primes = sieve(50000, true);
+var primes = sieve(30000);
 var primes_buffer = primes.splice(0, 5);
 while (!found) {
     var evaluated = primes.shift();
     console.log(evaluated);
-    var groups = primes_buffer.combinations(4); // combinations of four elements from primes_buffer excluding c
+    var groups = primes_buffer.combinations(4); // combinations of four elements from primes excluding c
     for (var j=0; j<groups.length; j++) {
         groups[j].push(evaluated); // add c to each group, as a result, get combinations of 5 elements from
-                                   // primes_buffer containing c
+                                   // primes containing c
         var combinations = groups[j].combinations(2); // combinations of 2 elements from group
         var all_primes = true;
         for (var i=0; i<combinations.length; i++) {
-            if (!is_prime("" + combinations[i][0] + combinations[i][1]) ||
-                !is_prime("" + combinations[i][1] + combinations[i][0])) {
+            var combination_1 = "" + combinations[i][0] + combinations[i][1];
+            var combination_2 = "" + combinations[i][1] + combinations[i][0];
+            if (!is_prime(combination_1) || !is_prime(combination_2)) {
                 all_primes = false;
                 break;
             }
