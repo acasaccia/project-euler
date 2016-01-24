82ec647ed2afb6a2c46dedb28f41d56a0a6fb3d5
Andrea Casaccia <acasaccia@gmail.com>
2016-01-24 12:58:05 +0000

    Changed concat to use string casting as it seem to perform better

--- javascript/lib/concat.js
+++ javascript/lib/concat.js
@@ -1,7 +1,5 @@
 var digits = require("./digits.js");
 
 module.exports = function concat(n1, n2) {
-    var length_n2 = digits(n2);
-    n1 *= Math.pow(10, length_n2);
-    return n1 + n2;
+    return parseInt("" + n1 + n2);
 };
