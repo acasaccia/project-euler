8f39b815577827e6d34d1f5be505f2e10dae4a0f
Andrea Casaccia <acasaccia@gmail.com>
2016-01-26 01:18:24 +0000

    Better documentation for problem 62 and 63

--- javascript/problems/63.js
+++ javascript/problems/63.js
@@ -1,9 +1,16 @@
+// The 5-digit number, 16807=75, is also a fifth power. Similarly, the 9-digit number, 134217728=89, is a ninth power.
+// How many n-digit positive integers exist which are also an nth power?
+
 var digits = require("../lib/digits.js");
+
 var total = 0;
 var powerful = [];
-for (var i=1; i<100; i++) {
+// after base 10 we won't find any, because the number of digit of x is floor(log(10) x) + 1
+for (var i=1; i<10; i++) {
     var ar = [];
-    for (var j=0; j<100; j++) {
+    // after number of digits is overcome by exponent, it won't "catch" it again because it will keep growing with log
+    // while exponent grows linearly
+    for (var j=1; j<50; j++) {
         var pow = Math.pow(i,j);
         if (digits(pow) === j) {
             total++;
@@ -15,5 +22,6 @@ for (var i=1; i<100; i++) {
     }
     console.log("%d -> %j", i, ar);
 }
+
 console.log(total);
 console.log(powerful);
