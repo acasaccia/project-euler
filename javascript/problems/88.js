e6e2946a2c95a6da31067886c39f8c7b1617c860
Andrea Casaccia <ANDREACA@amdocs.com>
2016-01-27 12:37:53 +0000

    Problem 88 description

--- javascript/problems/88.js
+++ javascript/problems/88.js
@@ -11,7 +11,7 @@
 // k=5: 8 = 1 × 1 × 2 × 2 × 2 = 1 + 1 + 2 + 2 + 2
 // k=6: 12 = 1 × 1 × 1 × 1 × 2 × 6 = 1 + 1 + 1 + 1 + 2 + 6
 //
-// Hence for 2?k?6, the sum of all the minimal product-sum numbers is 4+6+8+12 = 30; note that 8 is only counted once
+// Hence for 2>=k>=6, the sum of all the minimal product-sum numbers is 4+6+8+12 = 30; note that 8 is only counted once
 // in the sum.
-// In fact, as the complete set of minimal product-sum numbers for 2?k?12 is {4, 6, 8, 12, 15, 16}, the sum is 61.
-// What is the sum of all the minimal product-sum numbers for 2?k?12000?
+// In fact, as the complete set of minimal product-sum numbers for 2>=k>=12 is {4, 6, 8, 12, 15, 16}, the sum is 61.
+// What is the sum of all the minimal product-sum numbers for 2>=k>=12000?
