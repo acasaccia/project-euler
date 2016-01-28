5648a4b35ed79f1da7c67b852124ef55fd27b85c
Andrea Casaccia <ANDREACA@amdocs.com>
2016-01-28 10:47:27 +0000

    Problem 98

--- javascript/problems/98.js
+++ javascript/problems/98.js
@@ -13,14 +13,19 @@
 // NOTE: All anagrams formed must be contained in the given text file.
 
 var digits = require("../lib/digits.js");
+var combinations = require("../lib/combinations.js");
 
 var words = require('fs').readFileSync('./javascript/problems/input/p098_words.txt', 'utf-8').split(',').map(function(w){
     return w.replace(/\"/g, "");
 });
 
 var anagrams = {};
+var max_length = 0;
 
 words.forEach(function(w){
+    if (w.length > max_length) {
+        max_length = w.length;
+    }
     var letters = w.split("");
     letters.sort();
     letters = letters.join("");
@@ -37,57 +42,68 @@ for (var i in anagrams) {
     }
 }
 
-var squares = [];
+var squares = {};
 var c = 0;
-var last = 0;
-while (digits(last) < 15) { // longest word is 14
-    last = c * c;
-    squares[c] = last;
+var square = 0;
+var square_digits = 0;
+while (square_digits < max_length + 1) {
+    square = c * c;
+    var square_digits = digits(square);
+    if (squares[square_digits] === undefined) {
+        squares[square_digits] = [ square ];
+    } else {
+        squares[square_digits].push(square);
+    }
     c++;
 }
 
 var result = 0;
 for (var i in anagrams) {
-    var c = 0;
-    var square_digits = digits(squares[c]);
-    while(square_digits <= i.length) {
-        if (square_digits === i.length) {
-            if (normalize(i).join(",") === normalize(squares[c]).join(",")) {
-                var mapped = map(i, squares[c]);
-                if (squares.indexOf(mapped)) {
-                    if (squares[c] > result) {
-                        console.log("New maximum: %j -> %j", squares[c], anagrams[i]);
-                        result = squares[c];
+    var digits = i.length;
+    for (var k=0; k<squares[digits].length; k++) {
+        anagrams[i].combinations(2).forEach(function(c){
+            var mapping = find_mapping(squares[digits][k], c[0]);
+            if (mapping) {
+                var candidate = apply_mapping(mapping, c[1]);
+                var match = squares[digits].indexOf(candidate);
+                if (match > -1) {
+                    console.log("Found match: %s <=> %s", c[0], c[1]);
+                    console.log("             %j <=> %j", squares[digits][k], squares[digits][match]);
+                    if (squares[digits][k] > result) {
+                        result = squares[digits][k];
+                    }
+                    if (squares[digits][match] > result) {
+                        result = squares[digits][match];
                     }
                 }
             }
-        }
-        square_digits = digits(squares[++c]);
+        });
     }
 }
 console.log(result);
 
-function normalize(n) {
-    n = "" + n;
-    var normalized = [];
-    while(n.length) {
-        var letter = n.charAt(0);
-        var occurrences = n.split(letter).length  - 1;
-        normalized.push(occurrences);
-        var regexp = new RegExp(letter, "g");
-        n = n.replace(regexp, "");
+function find_mapping(number, word) {
+    var number = "" + number;
+    var char_to_digit = {};
+    var digit_to_char = {};
+    for (var i=0; i<word.length; i++) {
+        var char = word.charAt(i);
+        var digit = number.charAt(i);
+        if (char_to_digit[char] !== undefined && char_to_digit[char] !== digit) {
+            return false;
+        }
+        if (digit_to_char[digit] !== undefined && digit_to_char[digit] !== char) {
+            return false;
+        }
+        char_to_digit[char] = digit;
+        digit_to_char[digit] = char;
     }
-    return normalized.sort();
+    return char_to_digit;
 }
 
-function map(from, to) {
-    to = "" + to;
-    for (var i=0; i<from.length; i++) {
-        var letter = from.charAt(i);
-        var number = parseInt(letter);
-        if (isNaN(number)) {
-            from = from.replace(new RegExp(letter, "g"), to.charAt(i));
-        }
+function apply_mapping(mapping, word) {
+    for (var char in mapping) {
+        word = word.replace(new RegExp(char, "g"), mapping[char]);
     }
-    return parseInt(from);
+    return parseInt(word);
 }
