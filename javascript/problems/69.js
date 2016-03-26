4794917d5db6ece7e114e5651c31e66e98a6efc3
Andrea Casaccia <acasaccia@gmail.com>
2016-03-26 19:24:13 +0000

    Problem 69 description

--- javascript/problems/69.js
+++ javascript/problems/69.js
@@ -1,8 +1,8 @@
-// Euler's Totient function, ?(n) [sometimes called the phi function], is used to determine the number of numbers less
+// Euler's Totient function, φ(n) [sometimes called the phi function], is used to determine the number of numbers less
 // than n which are relatively prime to n. For example, as 1, 2, 4, 5, 7, and 8, are all less than nine and relatively
-// prime to nine, ?(9)=6.
+// prime to nine, φ(9)=6.
 //
-// n	Relatively Prime	?(n)	n/?(n)
+// n	Relatively Prime	φ(n)	n/φ(n)
 // 2	1	                1	    2
 // 3	1,2                 2       1.5
 // 4	1,3                 2       2
@@ -13,6 +13,5 @@
 // 9	1,2,4,5,7,8         6       1.5
 // 10	1,3,7,9             4       2.5
 //
-// It can be seen that n=6 produces a maximum n/?(n) for n ? 10.
-// Find the value of n ? 1,000,000 for which n/?(n) is a maximum.
-
+// It can be seen that n=6 produces a maximum n/φ(n) for n ≤ 10.
+// Find the value of n ≤ 1,000,000 for which n/φ(n) is a maximum.
